5a979f2a94a2d020cc1c6452026ca30c0822fabc
Jean Domingues <jeandomingues752@gmail.com>
2020-09-03 15:16:33 -0300

    Criando o Skeleton

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,30 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
-import { Container } from './styles';
+import { Container } from "./styles";
 
-import MobileHeader from '../MobileHeader'
-import DesktopHeader from '../DesktopHeader'
-import LeftColumn from '../LeftColumn';
-import MiddleColumn from '../MiddleColumn';
-import RightColumn from '../RightColumn';
-import AdBanner from '../AdBanner';
+import MobileHeader from "../MobileHeader";
+import DesktopHeader from "../DesktopHeader";
+import LeftColumn from "../LeftColumn";
+import MiddleColumn from "../MiddleColumn";
+import RightColumn from "../RightColumn";
+import AdBanner from "../AdBanner";
 
 const Layout: React.FC = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(()=>{
+    setTimeout(() => {
+      // setIsLoading(false);
+    }, 1000);
+  }, [])
+
   return (
     <Container>
       <MobileHeader />
       <DesktopHeader />
 
       <span>
-        <AdBanner/>
+        <AdBanner />
       </span>
       <main>
-        <LeftColumn /> 
-        <MiddleColumn />
-        <RightColumn />
+        <LeftColumn isLoading={isLoading} />
+        <MiddleColumn isLoading={isLoading}  />
+        <RightColumn isLoading={isLoading}  />
       </main>
     </Container>
-  )
-}
+  );
+};
 
 export default Layout;
